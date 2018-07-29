ccc920304bc2e7f6e36b4efc8f9608ef228371ec
Brady <vanderbannond@gmail.com>
2018-07-28 23:27:27 -0700

    modal working needs styling

--- src/App.js
+++ src/App.js
@@ -5,9 +5,40 @@ import ClientDetailsForm from './components/ClientDetails'
 import ScopeWork from './components/ScopeWork'
 import FeesDates from './components/FeesDates'
 import SideBar from './components/SideBar'
+import Modal from 'react-modal'
+import ModalContent from './components/ModalContent'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showModal: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal () {
+    this.setState({ showModal: true });
+  }
+
+  closeModal () {
+    this.setState({ showModal: false });
+  }
+
   render() {
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+}
     return (
       <div>
         <TopNav/>
@@ -16,6 +47,13 @@ class App extends Component {
           <ClientDetailsForm/>
           <ScopeWork />
           <FeesDates/>
+          <button onClick={this.openModal}>Trigger Modal</button>
+          <Modal
+           isOpen={this.state.showModal}
+           style={customStyles}
+        >
+          <ModalContent closeModal={this.closeModal}/>
+        </Modal>
          </div>
       </div>
     );
---
+++ src/components/ModalContent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Modal from 'react-modal'
+
+class ModalContent extends Component {
+
+  render() {
+    return (
+      <div style={{width: "600px", height: "500px"}}>
+      <div><h2>Create Contract</h2></div>
+      <div>Recipient Eth Address <input></input></div>
+      <div>Message <textarea></textarea></div>
+      <div>Fee: 12.4534 ETH</div>
+      <div>Ethereum Network Fee: 0.003 ETH</div>
+      <button onClick={this.props.closeModal}>Close Modal</button>
+    </div>
+    );
+  }
+}
+
+export default ModalContent;
