aef02429ee60691860d144ea12ab6bc18c780095
Brady <vanderbannond@gmail.com>
2018-08-17 15:57:20 -0700

    messages formatted alternating left and right

--- src/components/Contract/MessageBox.js
+++ src/components/Contract/MessageBox.js
@@ -5,21 +5,35 @@ class MessageBox extends Component {
   render() {
     return (
       <div className="message-box">
-        <div className="message-contractor">
-          <div className="message-text">Hey, I'm sending you the contract we talked about last week</div>
+        <div className="msg-right">
+          <div></div>
+          <div className="message-contractor">
+            <div className="message-text">Hey, I'm sending you the contract we talked about last week</div>
+          </div>
         </div>
-        <div className="message-signed">
-          <span style={{color: "#4957B8", fontWeight: "bold" }}>Employer </span>
-          <span style={{color: "#717682"}}>signed agreement with 12.4534 ETH</span>
+        <div className="msg-center">
+          <div className="message-signed">
+            <span style={{color: "#4957B8", fontWeight: "bold" }}>Employer </span>
+            <span style={{color: "#717682"}}>signed agreement with 12.4534 ETH</span>
+          </div>
         </div>
-        <div className="message-employer">
-          All set, let's start. Let me know if you have any additional questions
+        <div className="msg-left">
+          <div className="message-employer">
+            All set, let's start. Let me know if you have any additional questions
+          </div>
+          <div></div>
         </div>
-        <div className="message-contractor">
-          Hey, website is ready you can check it at <span style={{fontWeight: "bold"}}>http://goksiuta.com/test1</span>
+        <div className="msg-right">
+          <div></div>
+          <div className="message-contractor">
+            Hey, website is ready you can check it here: <span style={{fontWeight:  "bold"}}>http://goksiuta.com/test1</span>
+          </div>
         </div>
-        <div className="message-employer">
-          Looks great, can you just make the logo a little bigger?
+        <div className="msg-left">
+          <div className="message-employer">
+            Looks great, can you just make the logo a little bigger?
+          </div>
+          <div></div>
         </div>
       </div>
     )
